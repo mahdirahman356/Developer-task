11c2d7bab0f5f54a27356b5580d90d1d85fef315
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-02-19 16:47:18 +0600

    added carousel section

---
+++ app.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".crad-slider", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
