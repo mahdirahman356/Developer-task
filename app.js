1fa1414a81e434b7c09c5bc40c1122fa395c19e3
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-02-20 20:04:02 +0600

    added a menu system for small device

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ var swiper = new Swiper(".crad-slider", {
       },
   });
 
+
+let menuList = document.getElementById('menuList')
+  menuList.style.maxHeight = '0px'
+
+  const opneMenu = () => {
+       if(menuList.style.maxHeight = '0px'){
+          menuList.style.maxHeight = '844px'
+       }
+       else{
+        menuList.style.maxHeight = '0px'
+       }
+  }
+
+  const closeMenu = () => {
+     if(menuList.style.maxHeight = '844px'){
+        menuList.style.maxHeight = '0px'
+     }else{
+        menuList.style.maxHeight = '844px'
+     }
+  }
